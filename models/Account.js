6f7fd1920ac9b1f70b09f33fdd73719c905218ea
Marc Bluemner <marc.bluemner@gmail.com>
2013-10-11 18:27:26 +0200

    second commit

--- models/Account.js
+++ models/Account.js
@@ -1,4 +1,18 @@
 
+define(['models/StatusCollection'], function(StatusCollection) {
+    var Account = Backbone.Model.extend({
+        urlRoot: '/accounts',
+
+        initialize: function() {
+            this.status         = new StatusCollection();
+            this.status.url     = '/accounts/' + this.id + '/status';
+            this.activity       = new StatusCollection();
+            this.activity.url   = '/accounts/' + this.id + '/activity';
+        }
+    });
+
+    return Account;
+});
 
 module.exports = function(config, mongoose, nodemailer){
     var crypto = require('crypto');
--- public/js/SocialNet.js
+++ public/js/SocialNet.js
@@ -1,11 +1,39 @@
 
+define(['router'], function(router) {
+    var initialize = function() {
+        checkLogin(runApplication);
+    };
 
-define(['views/index'], function(indexView){
-    var initialize = function(){
-        indexView.render();
-    }
+    var checkLogin = function(callback) {
+        $.ajax("/account/authenticated", {
+            method: "GET",
+            success: function(){
+                return callback(true);
+            },
+            error: function(data) {
+                return callback(false);
+            }
+        });
+    };
+
+    var runApplication = function(authenticated){
+        if (!authenticated) {
+            window.location.hash = 'login';
+        } else {
+            window.location.hash = "index";
+        }
+        Backbone.history.start();
+    };
 
     return {
         initialize: initialize
     };
 });
+
+
+
+
+
+
+
+
