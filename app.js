3d4fc8d3be31f76083e0ec62be141b1e41fb4085
Marc Bluemner <marc.bluemner@gmail.com>
2013-10-12 12:14:05 +0200

    latest commit 	modified:   app.js

--- app.js
+++ app.js
@@ -107,6 +107,46 @@ app.post('/resetPassword', function(req, res){
     res.render('resetPasswordSuccess.jade');
 });
 
+app.get('/accounts/:id', function(req, res){
+    var accountId = req.params.id == 'me'
+                        ? req.session.accountId
+                        : req.params.id;
+    Account.findOne({_id: accountId}, function(account) {
+        res.send(account);
+    });
+});
+
+app.get('/accounts/:id/status', function(req, res){
+    var accountId = req.params.id == 'me'
+                    ? req.session.accountId
+                    : req.params.id;
+    models.Account.findById(accountId, function(account) {
+        res.send(account.status);
+    });
+});
+
+app.post('/accounts/:id/status', function(req, res){
+    var accountId = req.params.id == 'me'
+                    ? req.session.accountId
+                    : req.params.id;
+    models.Account.findById(accountId, function(account) {
+        status = {
+            name: account.name,
+            status: req.param('status', '')
+        };
+        account.status.push(status);
+
+        // Push the status to all friends
+        account.activity.push(status);
+        account.save(function(err) {
+            if (err) {
+                console.log('Error saving account: ' + err);
+            }
+        });
+    });
+    res.send(200);
+});
+
 app.listen(8080);
 
 
